a851aa4ca23e32a870f72b4d9a9cbe3b3b2b4306
jhmin <jhmin915@gmail.com>
2025-05-03 15:57:15 +0900

    add css file

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,10 @@
 import MainPage from "@/pages/MainPage.vue";
+import WorkSpacePage from "@/pages/WorkSpacePage.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [ 
-    {path:'/' ,  component : MainPage } 
+    {path:'/' ,  component : MainPage },
+    {path:'/workspace', component : WorkSpacePage}
 ];
 const router = createRouter({
     history : createWebHistory() , 
