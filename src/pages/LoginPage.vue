29874b52b6d6b6d097e01d6f01341d82d95732c9
WhiteKoreanPasta <dyddmlwlq@gmail.com>
2025-05-10 15:49:28 +0900

    로그인 페이지 추가

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- 헤더 -->
+  <h1>Login Page</h1>
+  <main>
+    <!-- 로그인 제출 -->
+    <!-- id ~~> 이메일 -->
+    <!-- password -->
+    <form action="">
+      <div class="form-group">
+        <label for="email">이메일</label>
+        <input type="text" id="email" placeholder="이메일을 입력하세요.">
+      </div>
+      <div class="form-group">
+        <label for="password">비밀번호</label>
+        <input type="password" id="password" placeholder="비밀번호를 입력하세요.">
+      </div>
+      <!-- 제출 버튼 -->
+      <div class="form-group">
+        <button type="submit" class="effect-button">로그인</button>
+      </div>
+    </form>
+    <!-- 하단메뉴 -->
+    <div>
+      <p>계정이 없으신가요?<RouterLink>회원가입</RouterLink></p>
+      <p>비밀번호를 잊으셨나요?<RouterLink>비밀번호 찾기</RouterLink></p>
+    </div>
+  </main>
+</template>
+
+<script setup>
+</script>
+
+<style scoped></style>
--- src/routers/index.js
+++ src/routers/index.js
@@ -6,6 +6,7 @@ import UsageInputPage from "@/pages/UsageInputPage.vue";
 import { createRouter, createWebHistory  } from "vue-router";
 import WorkplaceList from '@/pages/WorkplaceList.vue';
 import WorkplaceForm from '@/pages/WorkplaceForm.vue';
+import LoginPage from "@/pages/LoginPage.vue";
 
 const routes = [ 
     {path:'/' ,  component : MainPage },
@@ -15,7 +16,8 @@ const routes = [
     { path: '/workplace/update/:workplaceId', component: WorkplaceForm, props: true },
     {path:'/workplace/:workplaceId', component : ResourceListPage},
     {path:'/workplace/:workplaceId/:resourceId/input', component : UsageInputPage},
-    { path: '/', redirect: '/workplaces' }
+    { path: '/', redirect: '/workplaces' },
+    { path: '/login', component : LoginPage }
 ];
 const router = createRouter({
     history : createWebHistory() , 
