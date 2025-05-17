2b996dd01384df107ad079e401b22da7122158a1
yoojin <irisban02@gmail.com>
2025-05-17 14:39:45 +0900

    resource update

--- src/routers/index.js
+++ src/routers/index.js
@@ -9,6 +9,7 @@ import WorkplaceForm from '@/pages/WorkplaceForm.vue';
 import UsageUpdatePage from "@/pages/UsageUpdatePage.vue";
 import UsageReadPage from "@/pages/UsageReadPage.vue";
 
+
 const routes = [ 
     {path:'/' ,  component : MainPage },
     //{path:'/workspace', component : WorkSpacePage},
@@ -16,9 +17,9 @@ const routes = [
     { path: '/workplace/new', component: WorkplaceForm },
     { path: '/workplace/update/:workplaceId', component: WorkplaceForm, props: true },
     {path:'/workplace/:workplaceId', component : ResourceListPage},
-    {path:'/workplace/:workplaceId/:resourceId', component : UsageReadPage},
-    {path:'/workplace/:workplaceId/:resourceId/input', component : UsageInputPage},
-    {path:'/workplace/:workplaceId/:resourceId/update', component : UsageUpdatePage},
+    {path:'/resource/:resourceId', component : UsageReadPage},
+    {path:'/resource/:resourceId/input', component : UsageInputPage},
+    {path:'/resource/:resourceId/update', component : UsageUpdatePage},
     { path: '/', redirect: '/workplaces' }
 ];
 const router = createRouter({
