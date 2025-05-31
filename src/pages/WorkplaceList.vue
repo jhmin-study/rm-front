5238a61aecc81b104744458c51ca695ce62945f5
jhmin <jhmin915@gmail.com>
2025-05-31 15:42:15 +0900

    등록된 사업장이 없을 경우 문구표시

--- src/pages/WorkplaceList.vue
+++ src/pages/WorkplaceList.vue
@@ -2,6 +2,9 @@
   <div>
     <h2>사업장 목록</h2>
     <button @click="goToCreatePage">새 사업장 등록</button>
+    <div v-show = "workplaces.length == 0">
+        등록된 사업장이 없습니다. 새로 등록해주세요
+    </div>
     <div class="card-container">
       <div class="workplace-card" v-for="workplace in workplaces" :key="workplace.workplaceId">
         <h3>{{ workplace.businessName }}</h3>
