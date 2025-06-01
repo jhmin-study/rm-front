7e9a4bb28ed9801b067801954677137ccebc0917
yoojin <irisban02@gmail.com>
2025-06-01 16:21:40 +0900

    수정

--- src/pages/UsageInputPage.vue
+++ src/pages/UsageInputPage.vue
@@ -1,7 +1,15 @@
 <template>
 <!--header-->
 <main>
-<UsageComponent :is-edit="false"/>
+  <div v-if="status=='loading'" class="loading">
+    loading...
+  </div>
+  <div v-else-if="status=='not found'" class="not-found">
+    Not Found
+  </div>
+  <div v-else class="complete">
+    <UsageComponent :is-edit="false" :resource-info="resourceInfo"/>
+  </div>
 </main>
 <!--footer---->
 </template>
@@ -9,7 +17,26 @@
 <script setup>
 
 import UsageComponent from '@/assets/components/UsageComponent.vue';
+import axios from 'axios';
+import { ref } from 'vue';
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 
+const route = useRoute();
+const resourceInfo = ref(null);
+const status = ref("loading");
+
+onMounted(async()=>{
+  const res = await axios.get(`http://localhost:8003/api/resource/${route.params.resourceId}`);
+  console.log(res);
+  if(res.data == null || res.data == ''){
+    status.value = "not found";
+    return;
+  }
+  resourceInfo.value = res.data;
+  console.log(resourceInfo.value);
+  status.value = "completed"
+})
 </script>
 
 <style scoped>
--- src/pages/UsageReadPage.vue
+++ src/pages/UsageReadPage.vue
@@ -34,37 +34,38 @@
       {{ usageNresourceInfo.resourceUsage.resourceUserNote }}
     </div>
     <div class="usageSt">
-      <p>사용 시작일 입력</p>
+      <p>사용 시작일</p>
       {{ usageNresourceInfo.resourceUsage.usageSt }}
-      <p>사용 종료일 입력</p>
+      <p>사용 종료일</p>
       {{ usageNresourceInfo.resourceUsage.usageEd }}
     </div>
   </div>
   <button><RouterLink :to="`/resource/${resourceId}/input`">사용자 명단 추가</RouterLink></button>
-  <table v-if="status2">
-    <thead>
-      <tr>
-        <th>Status</th>
-        <th>User Name</th>
-        <th>User Phone</th>
-        <th>User Email</th>
-        <th>User Note</th>
-        <th>Start</th>
-        <th>End</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr  v-for="future in futureUsageInfo" :key="future.usageId">
-        <th>{{ future.usageStatus  }}</th>
-        <th>{{ future.resourceUserName }}</th>
-        <th>{{  future.resourceUserPhone }}</th>
-        <th>{{ future.resourceUserEmail }}</th>
-        <th>{{ future.resourceUserNote }}</th>
-        <th>{{ future.usageSt }}</th>
-        <th>{{ future.usageEd }}</th>
-      </tr>
-    </tbody>
-  </table>
+  <div class="reservation"  v-if="status2">
+    <p>예약자 명단</p>
+    <table>
+      <thead>
+        <tr>
+          <th>User Name</th>
+          <th>User Phone</th>
+          <th>User Email</th>
+          <th>User Note</th>
+          <th>Start</th>
+          <th>End</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr  v-for="future in futureUsageInfo" :key="future.usageId">
+          <td>{{ future.resourceUserName }}</td>
+          <td>{{  future.resourceUserPhone }}</td>
+          <td>{{ future.resourceUserEmail }}</td>
+          <td>{{ future.resourceUserNote }}</td>
+          <td>{{ future.usageSt }}</td>
+          <td>{{ future.usageEd }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </main>
 <!--footer-->
 </template>
@@ -88,13 +89,13 @@ onMounted(
     try{
       let res = await axios.get(`http://localhost:8003/api/resourceUsage/${route.params.resourceId}`)
       usageNresourceInfo.value = res.data;
-      console.log( res.data)
+      console.log(res.data)
       if(usageNresourceInfo.value =='null'||usageNresourceInfo.value ==''){
         status.value = "Not Found";
         return;
       }
       status.value = "loaded";
-      let sul = await axios.get(`http://localhost:8003/api/futureUsage/${route.params.resourceId}`)
+      let sul = await axios.get(`http://localhost:8003/api/resource/${route.params.resourceId}/futureUsage`)
       console.log(sul.data);
       futureUsageInfo.value = sul.data;
       if(futureUsageInfo.value != null && futureUsageInfo.value != ''){
@@ -109,5 +110,94 @@ onMounted(
 </script>
 
 <style scoped>
+body {
+  padding: 1.5em;
+  background: #f5f5f5
+}
 
+table {
+  border: 1px #a39485 solid;
+  font-size: .9em;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, .25);
+  width: 100%;
+  border-collapse: collapse;
+  overflow: hidden;
+}
+
+th {
+  text-align: left;
+}
+
+thead {
+  font-weight: bold;
+  color: #000000;
+  background: #ffffff;
+  border-top: #000000 solid 2px;
+  border-bottom: #000000 solid 2px;
+}
+
+td,
+th {
+  padding: 1em .5em;
+  vertical-align: middle;
+}
+
+td {
+  border-bottom: 1px solid rgba(0, 0, 0, .1);
+  background: #fff;
+}
+
+a {
+  color: #73685d;
+}
+
+@media all and (max-width: 768px) {
+
+  table,
+  thead,
+  tbody,
+  th,
+  td,
+  tr {
+    display: block;
+  }
+
+  th {
+    text-align: right;
+  }
+
+  table {
+    position: relative;
+    padding-bottom: 0;
+    border: none;
+    box-shadow: 0 0 10px rgba(0, 0, 0, .2);
+  }
+
+  thead {
+    float: left;
+    white-space: nowrap;
+  }
+
+  tbody {
+    overflow-x: auto;
+    overflow-y: hidden;
+    position: relative;
+    white-space: nowrap;
+  }
+
+  tr {
+    display: inline-block;
+    vertical-align: top;
+  }
+
+  th {
+    border-bottom: 1px solid #a39485;
+  }
+
+  td {
+    border-bottom: 1px solid #e5e5e5;
+  }
+
+
+}
 </style>
